ced65a96cf8a924fe86450969418dc82e9c3e587
allpower2slaves <allpower2slaves@users.noreply.github.com>
2025-07-11 16:10:17 +0300

    added clickurl patch

--- st.h
+++ st.h
@@ -36,6 +36,7 @@ enum glyph_attribute {
 	ATTR_WDUMMY     = 1 << 11,
 	ATTR_SELECTED   = 1 << 12,
 	ATTR_BOLD_FAINT = ATTR_BOLD | ATTR_FAINT,
+	ATTR_URL	= 1 << 14,
 };
 
 enum selection_mode {
@@ -110,6 +111,10 @@ void selextend(int, int, int, int);
 int selected(int, int);
 char *getsel(void);
 
+void highlighturls(void);
+void unhighlighturls(void);
+void followurl(int, int);
+
 size_t utf8encode(Rune, char *);
 
 void *xmalloc(size_t);
@@ -129,3 +134,7 @@ extern unsigned int tabspaces;
 extern unsigned int defaultfg;
 extern unsigned int defaultbg;
 extern unsigned int defaultcs;
+extern char *urlhandler;
+extern char urlchars[];
+extern char *urlprefixes[];
+extern int nurlprefixes;
